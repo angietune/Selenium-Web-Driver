286f043563262ce47ac0453fdf1f102d579f839d
Nathalie <75065099+angietune@users.noreply.github.com>
2021-05-28 20:26:35 +0300

    init: Lab4

---
+++ AbstractPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace Lab4
+{
+    abstract class AbstractPage
+    {
+        protected IWebDriver driver;
+    }
+}
